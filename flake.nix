58dc7b4ede06623b0bb3acd402dc35def9aeb7fc
Boris Glavic <lordpretzel@gmail.com>
2024-03-30 16:13:46 -0500

    flake

--- flake.nix
+++ flake.nix
@@ -5,36 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    doom-emacs = {
+      url = "github:doomemacs/doomemacs";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-doom-emacs, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, doom-emacs, ... }@inputs:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          doom-emacs = nix-doom-emacs.packages.${system}.default.override {
-            doomPrivateDir = ./.doom.d;
-          };
+          # doom-emacs = nix-doom-emacs.packages.${system}.default.override {
+          #   doomPrivateDir = ./.doom.d;
+          # };
+          doomemacsdir = "doomemacsdir";
         in
           {
             devShells.default = pkgs.mkShell {
-              buildInputs = [
-                doom-emacs
-                pkgs.fzf
-                pkgs.gnugrep
-                pkgs.neofetch
-                pkgs.git
-                pkgs.gnumake
-                pkgs.eza
+              buildInputs = with pkgs; [
+                emacs29
+                fzf
+                gnugrep
+                neofetch
+                git
+                gnumake
+                eza
               ];
 
               shellHook = ''
+        EMACS=${pkgs.emacs29}/bin/emacs
         source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
         GIT_PS1_SHOWDIRTYSTATE=true
         GIT_PS1_SHOWUNTRACKEDFILES=true
         GIT_DISCOVERY_ACROSS_FILESYSTEM=true
         source ${self}/shellsetup.sh
         source ${pkgs.fzf}/share/fzf/key-bindings.bash
+        if [ ! -f ~/doomemacsdir ]; then
+           cp -r ${doom-emacs}/ ~/${doomemacsdir}/
+           cp -r ${self}/.doom.d/ ~/.doom.d/
+           export PATH=~/${doomemacsdir}/bin:$PATH
+           ~/${doomemacsdir}/bin/doom install --emacsdir ~/${doomemacsdir}
+        fi
+        export PATH=${doomemacsdir}/bin:$PATH
+        alias doomemacs="${pkgs.emacs29}/bin/emacs --init-directory "$HOME/${doomemacsdir}"
         '';
             };
           }
