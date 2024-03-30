0db2cdadbc7c03843533d19d0a8deac36fc854d3
Boris Glavic <lordpretzel@gmail.com>
2024-03-30 15:40:58 -0500

    uinset LV_ALL

--- shellsetup.sh
+++ shellsetup.sh
@@ -30,8 +30,9 @@ export MYOS
 export LANG=en_US.UTF-8
 if [[ "$OSTYPE" != "linux-gnu" ]]; then
     export LC_ALL=en_US.UTF-8
+else
+    unset LC_ALL
 fi
-
 if
     [ -x "$(command -v exa)" ]; then
     alias l="exa -a --group-directories-first --color=always"
